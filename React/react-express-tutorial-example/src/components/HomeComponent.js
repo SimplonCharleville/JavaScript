fee4e7212d21f1fc0df7a75d846de6274cbaa05f
Simplon Charleville <38696381+SimplonCharleville@users.noreply.github.com>
2018-10-08 09:16:19 +0200

    Add files via upload

---
+++ React/react-express-tutorial-example/src/components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class HomeComponent extends Component {
+    render() {
+      return (
+        <div className="container">
+          <h1 className="mgnTop50">Server management App</h1>
+          <p>Click the navigation items to manage servers</p>
+        </div>
+      );
+    }
+  }
